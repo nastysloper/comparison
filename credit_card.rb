6ced9e589eaa6500bf566ac62435beccc99eba34
Richard Vogt <richvogtjr@gmail.com>
2013-09-08 12:04:00 -0500

    Implemented check card method.  Have not yet split apart 2-digit numbers.

--- credit_card.rb
+++ credit_card.rb
@@ -11,7 +11,29 @@ class CreditCard
   end
   
   def check_card
+    ints = numbered.map { | element | element.to_i }
+    doubleds = []
+    ints.each_with_index do | element, index |
+      if index % 2 == 0
+        doubleds << element * 2
+      else
+        doubleds << element
+      end
+    end
+
     
+    
+    sum = doubleds.reduce(:+)
+    p sum
+    print "\n"
+    if sum % 10 == 0
+      puts "Valid!"
+    else
+      puts "No gud!"
+    end
   end
 
 end
+
+my_card = CreditCard.new(1111_1111_1111_1112)
+my_card.check_card
